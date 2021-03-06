88c9d2c9aaf33ce62d700b15b5ad85274bb7842b
mnadirch <78723289+mnadirch@users.noreply.github.com>
2021-03-07 01:43:03 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main( ){
+	int number ;
+	cout<<"Enter any number:";
+	cin>>number;
+
+	if(number%2==0){
+		cout<<"Even number.";
+	}
+	else{
+		cout<<"Odd number.";
+	}
+	
+	return 0;
+}
+
